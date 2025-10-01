952b15511e99663d8acc6368f4fd86fe9e4e489e
venkata-naveen-varma <venkata2naveen@gmail.com>
2025-10-01 15:11:23 -0400

    PatientService

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,10 +1,12 @@
 package com.pm.patientservice.service;
 
 import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,5 +20,6 @@ public class PatientService {
     public List<PatientResponseDTO> getPatients() {
         List<Patient> patients = patientRepository.findAll();
 
+        return patients.stream().map(PatientMapper::toDTO).toList();
     }
 }

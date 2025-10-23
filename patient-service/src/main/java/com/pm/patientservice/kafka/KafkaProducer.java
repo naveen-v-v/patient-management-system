93f5f95feaa1ab5fee4b57aa4472632538163eeb
venkata-naveen-varma <venkata2naveen@gmail.com>
2025-10-23 09:22:08 -0400

    kafka Producer

---
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -0,0 +1,9 @@
+package com.pm.patientservice.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+
+public class KafkaProducer {
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+
+}

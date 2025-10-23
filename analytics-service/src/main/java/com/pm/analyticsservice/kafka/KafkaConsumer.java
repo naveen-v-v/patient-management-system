d31d8429fe788c269f45ba3fb4f799de881d2ebe
venkata-naveen-varma <venkata2naveen@gmail.com>
2025-10-23 13:07:01 -0400

    analytics-service kafka consumer bug fixes

--- analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
+++ analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
@@ -4,8 +4,10 @@ import com.google.protobuf.InvalidProtocolBufferException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 import patient.events.PatientEvent;
 
+@Service
 public class KafkaConsumer {
     private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
 

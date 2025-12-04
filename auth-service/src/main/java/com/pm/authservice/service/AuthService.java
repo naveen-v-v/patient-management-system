344207e885d57321399dd2e4567530c70a00eaee
venkata-naveen-varma <venkata2naveen@gmail.com>
2025-12-04 09:44:02 -0500

    authService-token chain operations

--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -8,8 +8,18 @@ import java.util.Optional;
 
 @Service
 public class AuthService {
-    public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
-        Optional<User> user = userService.findByEmail(loginRequestDTO.getEmail());
 
+    // dependency injection of UserService
+    private UserService userService;
+    public AuthService(UserService userService) {
+        this.userService = userService;
+    }
+
+    public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
+        Optional<String> token = userService.findByEmail(loginRequestDTO.getEmail())
+                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(), u.getPassword()))
+                .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
+
+        return token;
     }
 }
--- auth-service/src/main/java/com/pm/authservice/service/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pm.authservice.service;
-
-import com.pm.authservice.model.User;
-import org.springframework.data.repository.Repository;
-
-import java.util.UUID;
-
-interface UserRepository extends Repository<User, UUID> {
-}
